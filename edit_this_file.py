bc351c5fe7fd74550bb75e104a4358dd8fbd618f
Shuby Deshpande <shubhankardeshpande@hotmail.com>
2019-06-19 12:32:43 -0700

    Revert to loading image from train file (instead of static path)

--- edit_this_file.py
+++ edit_this_file.py
@@ -57,14 +57,14 @@ model = create_locloss_model(input_shape, anchors, num_classes, freeze_body=2, w
 # model.compile(optimizer=Adam(lr=1e-3), loss={'yolo_loss': lambda y_true, y_pred: y_pred})
 
 sess = K.get_session()
-model_image_size = (416,416)
+# model_image_size = (416,416)
 
-img = "data/5cc3a5ef4e436f43f7b5615f/images/0.jpeg"
-image = Image.open(img)
-boxed_image = letterbox_image(image, tuple(reversed(model_image_size)))
-image_data = np.array(boxed_image, dtype='float32')
-image_data /= 255.
-image_data = np.expand_dims(image_data, 0)
+# img = "data/5cc3a5ef4e436f43f7b5615f/images/0.jpeg"
+# image = Image.open(img)
+# boxed_image = letterbox_image(image, tuple(reversed(model_image_size)))
+# image_data = np.array(boxed_image, dtype='float32')
+# image_data /= 255.
+# image_data = np.expand_dims(image_data, 0)
 # input_image_shape = K.placeholder(shape=(2, ))
 
 # retrieve_layers = [-4,-3,-2]
