185d9cd262bda01e31d3dd7996c7351d175591f4
Shuby Deshpande <shubhankardeshpande@hotmail.com>
2019-07-08 21:43:48 -0700

    cleanup dump qa reports

--- dump_qa_reports.py
+++ dump_qa_reports.py
@@ -1,18 +1,21 @@
 import json
 
-f = json.load(open('qa_report.json'))
-frames = []
+def ret_mapping(qa_report):
+    f = json.load(open(qa_report))
+    frames = []
+    for i in range(len(f['nuro_labels'])): 
+        cam = f['nuro_labels'][i] 
+        for j in range(len(cam['frames'])): 
+            frames.extend(cam['frames'])
+    mapping = {}
+    for fn in range(len(frames)):
+        for annotation in range(len(frames[fn])):
+            try:
+                mapping[frames[fn]['annotations'][annotation]['scale_id']] = frames[fn]['annotations'][annotation]['bbox'] 
+            except:
+                continue
+    return mapping
 
-for i in range(len(f['nuro_labels'])): 
-    cam = f['nuro_labels'][i] 
-    for j in range(len(cam['frames'])): 
-        frames.extend(cam['frames'])
-
-mapping = {}
-
-for fn in range(len(frames)):
-    for annotation in range(len(frames[fn])):
-        try:
-            mapping[frames[fn]['annotations'][annotation]['scale_id']] = frames[fn]['annotations'][annotation]['bbox'] 
-        except:
-            continue
+if __name__ == "__main__":
+    qa_report = 'qa_report.json'
+    mapping = ret_mapping(qa_report)
